ff01f9ed5796fd3c7157c8827b3afe1bfc6c9e60
manuel borowski <emmanuel.borowski@gmail.com>
2024-11-30 11:13:03 +0100

    0.77: firefox specific update

--- app/__init__.py
+++ app/__init__.py
@@ -136,8 +136,9 @@ flask_app.config.from_pyfile('config.py')
 # 0.74: small bugifx
 # 0.75: auto-reload page to reset socketio
 # 0.76: small updates in auto-reload
+# 0.77: firefox specific update
 
-version = "0.76"
+version = "0.77"
 
 db = SQLAlchemy()
 login_manager = LoginManager()
